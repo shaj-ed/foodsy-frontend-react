ad0a151b29e49f205e1d412832c5e3f60e1e93a4
shaj-ed <shajedsr@gmail.com>
2025-12-28 01:50:27 +0600

    Chore(Input Field): added type and beatify withprettierr

--- src/components/common/form/RHFInput.tsx
+++ src/components/common/form/RHFInput.tsx
@@ -1,44 +1,52 @@
-import { Control, Controller, FieldValues, Path } from "react-hook-form";
-import { Field, FieldError, FieldLabel } from "../../ui/field";
-import { Input } from "../../ui/input";
+import { Control, Controller, FieldValues, Path } from 'react-hook-form';
+import { Field, FieldError, FieldLabel } from '../../ui/field';
+import { Input } from '../../ui/input';
 
 type RHFInputProps<T extends FieldValues> = {
-    name: Path<T>
-    control: Control<T>
-    label: string
-    placeholder?: string
-    autoComplete?: string
-    classname?: string
-}
+  name: Path<T>;
+  control: Control<T>;
+  label: string;
+  placeholder?: string;
+  autoComplete?: string;
+  classname?: string;
+  type?: string;
+};
 
-const RHFInput = <T extends FieldValues>({name, control, label, placeholder = "Enter value", autoComplete = "off", classname}: RHFInputProps<T>) => {
-    return (
-        <Controller
-            name={name}
-            control={control}
-            render={({field, fieldState: {invalid, error}}) => (
-                <Field data-invalid={invalid} className={classname}>
-                    <FieldLabel className="text-md" htmlFor={name}>{label}</FieldLabel>
+const RHFInput = <T extends FieldValues>({
+  name,
+  control,
+  label,
+  placeholder = 'Enter value',
+  autoComplete = 'off',
+  classname,
+  type = 'text',
+}: RHFInputProps<T>) => {
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field, fieldState: { invalid, error } }) => (
+        <Field data-invalid={invalid} className={classname}>
+          <FieldLabel className="text-md" htmlFor={name}>
+            {label}
+          </FieldLabel>
 
-                    <Input
-                        {...field}
-                        id={name}
-                        value={field.value}
-                        aria-invalid={invalid}
-                        placeholder={placeholder}
-                        autoComplete={autoComplete}
-                        className="border-gray-500"
-                    />
+          <Input
+            {...field}
+            type={type}
+            id={name}
+            value={field.value}
+            aria-invalid={invalid}
+            placeholder={placeholder}
+            autoComplete={autoComplete}
+            className="border-gray-500"
+          />
 
-                    {
-                        invalid && (
-                            <FieldError className="text-start" errors={[error]} />
-                        )
-                    }
-                </Field>
-            )}
-        />
-    )
-}
+          {invalid && <FieldError className="text-start" errors={[error]} />}
+        </Field>
+      )}
+    />
+  );
+};
 
-export default RHFInput
+export default RHFInput;
