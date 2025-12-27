aa3f49bc1261d91e4cd09f13baeb3f2135e09a22
shaj-ed <shajedsr@gmail.com>
2025-12-28 01:45:56 +0600

    Chore(category list): loading, error and data not available component

--- src/features/category/components/CategoryList.tsx
+++ src/features/category/components/CategoryList.tsx
@@ -1,20 +1,25 @@
-import { useCategoryList } from "../hooks/category.query"
-import CategoryCard from "./CategoryCard"
+import ContentLoading from '@/components/common/ui/ContentLoading';
+import { useCategoryList } from '../hooks/category.query';
+import CategoryCard from './CategoryCard';
+import AlertOneLiner from '@/components/common/ui/AlertOneLiner';
+import ContentError from '@/components/common/ui/ContentError';
 
 const CategoryList = () => {
-    const {data, isLoading} = useCategoryList()
+  const { data, isLoading, error } = useCategoryList();
 
-    if(isLoading) return <p>Loading..</p>
+  if (isLoading) return <ContentLoading />;
 
-    if(!data) return <p>No category found</p>
+  if (error) return <ContentError />;
 
-    return (
-        <section className="mt-5 grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-            {
-                data.map(item => <CategoryCard key={item.id} category={item} />)
-            }
-        </section>
-    )
-}
+  if (!data) return <AlertOneLiner title="No Category Found" />;
 
-export default CategoryList
+  return (
+    <section className="mt-5 grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+      {data.map((item) => (
+        <CategoryCard key={item.id} category={item} />
+      ))}
+    </section>
+  );
+};
+
+export default CategoryList;
