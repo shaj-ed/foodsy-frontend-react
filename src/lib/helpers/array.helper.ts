a62dea0197c19d4db80bb189ff1fb839e93851bc
shaj-ed <shajedsr@gmail.com>
2026-01-04 01:09:32 +0600

    Chore: helper get a value form and array of object and file helper beatify

--- src/lib/helpers/array.helper.ts
+++ src/lib/helpers/array.helper.ts
@@ -13,3 +13,13 @@ export const mapToSelectOptions = <T>(
     value: String(value[valueKey]),
   }));
 };
+
+// Get key name value from an array by id
+export const getValueById = <T extends { id: number }, K extends keyof T>(
+  data: T[],
+  id: number,
+  key: K
+): T[K] | '' => {
+  const selected = data.find((d) => d.id === id);
+  return selected ? selected[key] : '';
+};
