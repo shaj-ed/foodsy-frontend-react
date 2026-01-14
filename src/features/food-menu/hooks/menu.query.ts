b001ce1132a06c7cd4989916e8e80050594e47f6
shaj-ed <shajedsr@gmail.com>
2026-01-15 02:48:29 +0600

    feat(api): implement hooks for get product list and images

--- src/features/food-menu/hooks/menu.query.ts
+++ src/features/food-menu/hooks/menu.query.ts
@@ -1,5 +1,13 @@
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import { addMenu, deleteMenuById, getMenuList, updateMenu, uploadMenuFiles } from '../api/menu.api';
+import {
+  addMenu,
+  deleteMenuById,
+  getMenuById,
+  getMenuImagesById,
+  getMenuList,
+  updateMenu,
+  uploadMenuFiles,
+} from '../api/menu.api';
 import {
   MenuFilter,
   MenuPayload,
@@ -64,4 +72,40 @@ export const useDeleteMenu = () => {
     mutationFn: deleteMenuById,
     onSuccess: () => queryClient.invalidateQueries({ queryKey: ['menus'] }),
   });
+};
+
+export const useMenuDetails = (menuId?: number) => {
+  return useQuery({
+    queryKey: ['menu-details', menuId],
+    queryFn: () => getMenuById(menuId as number),
+    enabled: Number.isFinite(menuId),
+
+    staleTime: 0,
+    refetchOnMount: true,
+    refetchOnWindowFocus: false,
+  });
+};
+
+export const useMenuImages = (menuId?: number) => {
+  return useQuery({
+    queryKey: ['menu-images', menuId],
+    queryFn: () => getMenuImagesById(menuId as number),
+    enabled: Number.isFinite(menuId),
+
+    staleTime: 0,
+    refetchOnMount: true,
+    refetchOnWindowFocus: false,
+  });
+};
+
+export const useMenuDetailsPages = (menuId?: number) => {
+  const productQuery = useMenuDetails(menuId);
+  const imagesQuery = useMenuImages(menuId);
+
+  return {
+    isLoading: productQuery.isLoading || imagesQuery.isLoading,
+    error: productQuery.error || imagesQuery.error,
+    images: imagesQuery.data,
+    product: productQuery.data,
+  };
 };
