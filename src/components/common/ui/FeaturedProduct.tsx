f13b5a8b48420b1eab8f9c0527a387c1bf3f6aaa
shaj-ed <shajedsr@gmail.com>
2026-01-15 02:53:43 +0600

    feat(menu): api integration individual menu with similar menus

--- src/components/common/ui/FeaturedProduct.tsx
+++ src/components/common/ui/FeaturedProduct.tsx
@@ -1,12 +1,56 @@
+import { useMenuList } from '@/features/food-menu/hooks/menu.query';
 import SectionHeading from '../section/SectionHeading';
+import CardSkeleton from './CardSkeleton';
+import AlertOneLiner from './AlertOneLiner';
+import MenuCard from '@/features/food-menu/components/MenuCard';
 
-const FeaturedProduct = () => {
+type FeaturedProductProps = {
+  categoryId: number;
+  currentMenuId: number;
+};
+
+const FeaturedProduct = ({ categoryId, currentMenuId }: FeaturedProductProps) => {
+  const { data, isLoading, isError } = useMenuList(0, 4, categoryId);
+
+  if (isLoading)
+    return (
+      <>
+        <SectionHeading headline="More Foods" />
+        <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {Array.from({ length: 4 }).map((_, index) => (
+            <CardSkeleton key={index + '12'} />
+          ))}
+        </div>
+      </>
+    );
+
+  if (isError)
+    return (
+      <>
+        <SectionHeading headline="More Foods" />
+        <AlertOneLiner title="Failed to load similar product" />
+      </>
+    );
+
+  const menus = data?.data.filter((menu) => menu.id !== currentMenuId);
+
+  if (!menus || menus?.length === 0)
+    return (
+      <>
+        <SectionHeading headline="More Foods" />
+        <AlertOneLiner title="Similar product not available" />
+      </>
+    );
+
   return (
-    <div>
+    <>
       <SectionHeading headline="More Foods" />
-      <div className="mb-4"></div>
-      {/* <Products /> */}
-    </div>
+      <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {menus.map((menu) => (
+          <MenuCard key={menu.id} menu={menu} />
+        ))}
+      </div>
+    </>
   );
 };
 
--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -1,47 +1,93 @@
-import SectionContainer from "@/components/common/section/SectionContainer";
-import fastfood from "@/assets/images/categories/fastfood.jpg";
-import { Star } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import FeaturedProduct from "@/components/common/ui/FeaturedProduct";
+import SectionContainer from '@/components/common/section/SectionContainer';
+import { Star } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import FeaturedProduct from '@/components/common/ui/FeaturedProduct';
+import { useMenuDetailsPages } from '@/features/food-menu/hooks/menu.query';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import ContentLoading from '@/components/common/ui/ContentLoading';
+import AlertOneLiner from '@/components/common/ui/AlertOneLiner';
+import { getMimeTypeFromBase64 } from '@/lib/helpers/file.helper';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
+import { useAuthStore } from '@/store/authStore';
+import { toast } from 'sonner';
 
 const ProductDetails = () => {
+  const { isAuthenticated } = useAuthStore();
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const location = useLocation();
+  const { categoryId } = location.state;
+  const productId = Number(id);
+
+  const { isLoading, product, images, error } = useMenuDetailsPages(productId);
+
+  const handleAddCart = () => {
+    if (!isAuthenticated) {
+      toast.info('Please Login/Sign up');
+      navigate('/auth/login');
+      return;
+    }
+
+    console.log(id);
+  };
+
+  if (isLoading) {
+    return (
+      <div className="min-h-[80vh]">
+        <ContentLoading />
+      </div>
+    );
+  }
+
+  if (!product) return <AlertOneLiner title="No Data Available" />;
+
+  if (error) return <AlertOneLiner title="Try again!" />;
+
   return (
     <>
       <div className="bg-slate-800 px-4 md:px-14 py-10 text-white">
         <SectionContainer className="my-8">
           <div className="flex gap-4 flex-col md:flex-row">
-            <div className="overflow-hidden rounded w-full max-w-96 border-2 border-sky-400">
-              <img
-                src={fastfood}
-                alt=""
-                className="w-full h-full object-cover"
-              />
+            <div className="rounded w-full max-w-96">
+              <Carousel>
+                <CarouselContent>
+                  {images?.data.map((img) => (
+                    <CarouselItem>
+                      <img
+                        src={`data:${getMimeTypeFromBase64(img)};base64,${img}`}
+                        alt={product.data.productName}
+                        className="w-full h-[300px] object-contain"
+                      />
+                    </CarouselItem>
+                  ))}
+                </CarouselContent>
+                <div className="mt-8 flex justify-center gap-4">
+                  <CarouselPrevious variant="ghost" className="static translate-y-0 z-50" />
+                  <CarouselNext variant="ghost" className="static translate-y-0" />
+                </div>
+              </Carousel>
             </div>
 
             <div>
-              <h2 className="text-3xl font-headline">Pasta </h2>
-              <p className="text-sm">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non,
-                excepturi quaerat eveniet necessitatibus pariatur fugit expedita
-                voluptatibus! Error deleniti ad officiis, magni unde molestiae,
-                asperiores minima minus expedita fugiat eaque.
-              </p>
+              <h2 className="text-3xl font-headline">{product.data.productName}</h2>
+              <p className="text-sm">{product.data.description}</p>
               <p className="text-lg font-medium mt-3">Price: &#2547; 500</p>
               <div className="flex gap-1 mt-6">
                 {Array.from({ length: 5 }).map((_, index) => {
-                  return (
-                    <Star
-                      key={index}
-                      size="16px"
-                      className="text-amber-300 fill-amber-200"
-                    />
-                  );
+                  return <Star key={index} size="16px" className="text-amber-300 fill-amber-200" />;
                 })}
               </div>
 
               <Button
                 size="lg"
-                className="uppercase bg-red-400 hover:bg-red-500 cursor-pointer mt-8"
+                className="uppercase bg-sky-700 hover:bg-sky-800 cursor-pointer mt-8"
+                onClick={handleAddCart}
               >
                 Add to cart
               </Button>
@@ -50,7 +96,7 @@ const ProductDetails = () => {
         </SectionContainer>
       </div>
       <SectionContainer className="my-8">
-        <FeaturedProduct />
+        <FeaturedProduct categoryId={categoryId} currentMenuId={productId} />
       </SectionContainer>
     </>
   );
